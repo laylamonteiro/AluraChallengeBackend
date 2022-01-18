adf152f1db751db86ac3b628c54d6f7226c7a303
laylindo <layla-sep@hotmai.com>
2022-01-17 22:57:04 -0300

    Add Services

---
+++ BudgetApp/src/main/java/com/laylamonteiro/BudgetApp/Service/DespesasService.java
@@ -0,0 +1,12 @@
+package com.laylamonteiro.BudgetApp.Service;
+
+import com.laylamonteiro.BudgetApp.DTO.DespesasResponseDTO;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
+public class DespesasService {
+    public List<DespesasResponseDTO> findAll() {
+    }
+}
