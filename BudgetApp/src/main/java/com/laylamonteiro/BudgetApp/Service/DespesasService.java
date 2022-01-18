3c51a31094756d281df2a18f0f1bf10d535aae5f
laylindo <layla-sep@hotmai.com>
2022-01-17 23:33:04 -0300

    Add methods to DespesasService

--- BudgetApp/src/main/java/com/laylamonteiro/BudgetApp/Service/DespesasService.java
+++ BudgetApp/src/main/java/com/laylamonteiro/BudgetApp/Service/DespesasService.java
@@ -1,12 +1,39 @@
 package com.laylamonteiro.BudgetApp.Service;
 
-import com.laylamonteiro.BudgetApp.DTO.DespesasResponseDTO;
+import com.laylamonteiro.BudgetApp.Entity.Despesas;
+import com.laylamonteiro.BudgetApp.Repository.DespesasRepository;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Data
 @Service
+@Transactional
 public class DespesasService {
-    public List<DespesasResponseDTO> findAll() {
+
+    @Autowired
+    final DespesasRepository repository;
+
+    public List<Despesas> findAll() {
+        return repository.findAll();
+    }
+
+    public Despesas findById(final Long id) {
+        return repository.getById(id);
+    }
+
+    public Despesas create(Despesas despesa) {
+        return repository.save(despesa);
+    }
+
+    public Despesas update(Despesas despesa) {
+        return null;
+    }
+
+    public void delete(Long id) {
+        return;
     }
 }
